fde7f0b6c03b6ebbd9a615f781d68c523910651f
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2025-09-16 08:25:29 -0300

    feat: eslint, commit-lint, husky, prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import prettierPlugin from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -16,7 +16,7 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      prettierConfig.configs.recommended,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -29,4 +29,4 @@ export default tseslint.config([
       'prettier/prettier': 'error',
     },
   },
-])
+]);
